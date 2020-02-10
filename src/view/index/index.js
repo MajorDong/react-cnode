e471abf3ec8c8c1dec7e5ad5f5912debe6d82267
MajorDong <odsts@outlook.com>
2020-02-11 07:56:08 +0800

    完成侧边导航和移动端适配

--- src/view/index/index.js
+++ src/view/index/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Link, } from "react-router-dom"
-import { Menu, Row, Col} from 'antd'
+import { Menu, Row, Col } from 'antd'
 import './index.scss'
 
+let tempArr = []
+for(let i =0; i < 100; i++){
+  tempArr.push(<li>这是第{i}项</li>)
+}
 class Index extends React.Component {
   constructor(props) {
     super(props);
@@ -11,29 +15,35 @@ class Index extends React.Component {
   render() {
     return (
       <Row id="index">
-        <Col md={6} className="menu">
-        <Menu>
-          <Menu.Item>
-            <Link to="/index/ask">全部</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/index/good">精华</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/index/ask">问题</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/index/share">分享</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/index/job">招聘</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to="/index/test">测试</Link>
-          </Menu.Item>
-        </Menu>
+        <Col md={5} >
+          <div className="menuWrap">
+            <Menu className="menu" style={{ minWidth: 156}} mode="vertical-left">
+              <Menu.Item>
+                <Link to="/index/ask">全部</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/index/good">精华</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/index/ask">问题</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/index/share">分享</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/index/job">招聘</Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to="/index/dev">测试</Link>
+              </Menu.Item>
+            </Menu>
+          </div>
         </Col>
-       
+        <Col md={19} >
+          <div className="art">
+            {tempArr}
+          </div>
+        </Col>
       </Row>
     );
   }
