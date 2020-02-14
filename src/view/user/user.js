4d5da54151bbe1b1d219baed27a46e8e0b48d6ec
MajorDong <odsts@outlook.com>
2020-02-14 10:33:18 +0800

    完成user组件 userInfo部分

--- src/view/user/user.js
+++ src/view/user/user.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Avatar, Row, Col, Card} from 'antd'
+import data from './data'
+import './user.scss'
 
+const dataStyle = {
+  color: '#448ef7'
+}
 class User extends React.Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
   render() { 
+    console.log(data)
+    let {loginname, avatar_url, score, create_at} = data.data
     return ( 
-      '用户详情'
+      <div className="userWrap">
+        <div className="userInfo">
+          <Avatar className="userAvatar" src={avatar_url}/>
+          <Row style={{fontSize:'16px', textAlign:'center'}}>
+            <Col md={8}>用户名:<span style={dataStyle}>{loginname}</span></Col>
+            <Col md={8}>积分:<span style={dataStyle}>{score}</span></Col>
+            <Col md={8}>注册时间:<span style={dataStyle}>{create_at.split('T')[0]}</span></Col>
+          </Row>
+        </div>
+        <Card
+          type="inner"
+          title="最近创建话题"
+        ></Card>
+      </div>
      );
   }
 }
