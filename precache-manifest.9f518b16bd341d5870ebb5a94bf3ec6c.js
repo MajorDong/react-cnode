d3f0bf01214ce0ab280f79c8292a398f3db341ca
MajorDong <odsts@outlook.com>
2020-03-08 12:53:00 +0800

    Updates

---
+++ precache-manifest.9f518b16bd341d5870ebb5a94bf3ec6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07300276cd3f8a3d292e85e1ed279e7",
+    "url": "/react-cnode/index.html"
+  },
+  {
+    "revision": "66d7a8780dfc4cf85492",
+    "url": "/react-cnode/static/css/2.077357ba.chunk.css"
+  },
+  {
+    "revision": "5ca28c8588778e294846",
+    "url": "/react-cnode/static/css/main.aab0255e.chunk.css"
+  },
+  {
+    "revision": "66d7a8780dfc4cf85492",
+    "url": "/react-cnode/static/js/2.57e496bb.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-cnode/static/js/2.57e496bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca28c8588778e294846",
+    "url": "/react-cnode/static/js/main.0c305a9b.chunk.js"
+  },
+  {
+    "revision": "c08392c8299a6ba17015",
+    "url": "/react-cnode/static/js/runtime-main.04f3bc9b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.627c9b1afc2b00e7d59cee76fd1cfee5.js"
+  "/react-cnode/precache-manifest.9f518b16bd341d5870ebb5a94bf3ec6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-cnode/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
