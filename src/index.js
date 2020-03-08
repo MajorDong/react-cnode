97bfecb7a3c71297502afeed2d7920b096d95b4b
MajorDong <odsts@outlook.com>
2020-03-08 13:30:04 +0800

    改用HashRouter完成部署

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import {createStore, applyMiddleware} from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
